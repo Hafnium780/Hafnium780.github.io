c21e7d7a878ea7b266f9560f113696253adf9ab4
Hafnium780 <97712621+Hafnium780@users.noreply.github.com>
2022-02-26 20:14:56 -0800

    Add files via upload

---
+++ gravity/Gravity.js
@@ -0,0 +1,152 @@
+let p = [];
+let allP = [];
+let G;
+let startX = 0;
+let startY = 0;
+let dragging = false;
+let speedDiv = 50;
+let massSlider;
+let paused;
+let collide;
+let moving;
+let rsquared;
+let GSlider;
+let affectsOthers;
+let bounceOnWalls;
+
+function setup() {
+  createCanvas(1600, 1200);
+  paused = createCheckbox("Pause (Space)", false);
+  collide = createCheckbox("Detect Collisions", false);
+  rsquared = createCheckbox("Gravity is proportional to 1/r^2", true);
+  GSlider = createSlider(1, 100, 20);
+  massSlider = createSlider(1, 400, 3);
+  moving = createCheckbox("Placed Object Will Move", true);
+  affectsOthers = createCheckbox("Placed Object Will Attract Others", true);
+  bounceOnWalls = createCheckbox("Bounce Off Walls", false);
+  background(0);
+}
+
+function mousePressed() {
+  if (mouseX > width || mouseX < 0 || mouseY > height || mouseY < 0) {
+    return;
+  }
+  dragging = true;
+  startX = mouseX;
+  startY = mouseY;
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    paused.elt.getElementsByTagName("input")[0].checked = (paused.elt.getElementsByTagName("input")[0].checked - 1) % 2;
+    return false;
+  }
+  else if (keyCode == 68) {
+    let record = 200;
+    let recordInd = -1;
+    for (let i = 0; i < allP.length; i++) {
+      if (pow(allP[i].pos.x - mouseX, 2) + pow(allP[i].pos.y - mouseY, 2) < record) {
+        record = pow(allP[i].pos.x - mouseX, 2) + pow(allP[i].pos.y - mouseY, 2);
+        recordInd = i;
+      }
+    }
+    if (recordInd != -1) {
+      let found = false;
+      for (let i = 0; i < p.length; i++) {
+        if (allP[recordInd] == p[i]) {
+          allP.splice(recordInd, 1);
+          p.splice(i, 1);
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        allP.splice(recordInd, 1);
+      }
+    }
+  }
+}
+
+function mouseReleased() {
+  if (!dragging) {
+    return;
+  }
+  dragging = false;
+  if (affectsOthers.checked()) {
+    p.push(new Planet(startX, startY, (mouseX - startX) / speedDiv, (mouseY - startY) / speedDiv, massSlider.value(), moving.checked()));
+    allP.push(p[p.length-1]);
+  }
+  else {
+    allP.push(new Planet(startX, startY, (mouseX - startX) / speedDiv, (mouseY - startY) / speedDiv, massSlider.value(), moving.checked()));
+  }
+}
+
+function draw() {
+  G = GSlider.value();
+  background(0, 10  );
+  if (dragging) {
+    stroke(255);
+    line(startX, startY, mouseX, mouseY);
+  }
+  if (!paused.checked()) {
+    for (let i = 0; i < allP.length; i++) {
+      for (let j = 0; j < p.length; j++) {
+        if (allP[i] == p[j] || !allP[i].enabled || !p[j].enabled) {
+          continue;
+        }
+        let p1 = allP[i];
+        let p2 = p[j];
+        
+        let dx = p1.pos.x - p2.pos.x;
+        let dy = p1.pos.y - p2.pos.y;
+        
+        let f = G * p1.mass * p2.mass;
+        if (rsquared.checked()) {
+          f /= (pow(dx, 2) + pow(dy, 2));
+        }
+        else {
+          f /= sqrt(pow(dx, 2) + pow(dy, 2));
+        }
+        
+        if (f / p1.mass > 1) {
+          f = 1 * p1.mass;
+        }
+  
+        let force = createVector((p1.pos.x - p2.pos.x), (p1.pos.y - p2.pos.y));
+        force.setMag(f);
+        stroke(255, 0, 0);
+        
+        
+        allP[i].addForce(force.mult(-1));
+        
+        if (collide.checked()) {
+          allP[i].collide(p[j]);
+        }
+      }
+    }
+  }
+  for (let i = 0; i < allP.length; i++) {
+    if (!paused.checked()) {
+      allP[i].update();
+    }
+    allP[i].show();
+  }
+  if (bounceOnWalls) {
+    for (let i = 0; i < allP.length; i++) {
+      if (allP[i].pos.x - allP[i].r < 0 || allP[i].pos.x + allP[i].r > width || allP[i].pos.y - allP[i].r < 0 || allP[i].pos.y + allP[i].r > height) {
+        let found = false;
+        for (let j = 0; j < p.length; j++) {
+          if (allP[i] == p[j]) {
+            allP.splice(i, 1);
+            p.splice(j, 1);
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          allP.splice(i, 1);
+        }
+      }
+    }
+  }
+}
---
+++ gravity/Planet.js
@@ -0,0 +1,64 @@
+class Planet {
+  constructor(x, y, vx, vy, mass, moving, pIndex) {
+    this.moving = moving;
+    this.mass = mass;
+    this.r = mass / 2;
+    this.pos = createVector(x, y);
+    this.vel = createVector(vx, vy);
+    this.acc = createVector(0, 0);
+    this.enabled = true;
+  }
+  
+  update() {
+    if (this.moving && this.enabled) {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+    }
+    this.acc.mult(0);
+    if (bounceOnWalls.checked()) {
+      if (this.pos.x - this.r < 0 || this.pos.x + this.r > width) {
+        this.pos.sub(this.vel);
+        this.vel.x = -this.vel.x;
+        this.pos.add(this.vel);
+      }
+      if (this.pos.y - this.r < 0 || this.pos.y + this.r > height) {
+        this.pos.sub(this.vel);
+        this.vel.y = -this.vel.y;
+        this.pos.add(this.vel);
+      }
+    }
+  }
+  
+  show() {
+    if (this.enabled) {
+      stroke(255, 255);
+      fill(255);
+      if (this.mass < 0) {
+        fill(255, 0, 0);
+      }
+      ellipse(this.pos.x, this.pos.y, this.r*2);
+    }
+  }
+  
+  addForce(f) {
+    this.acc.add(f.mult(1/this.mass));
+  }
+  
+  collide(planet) {
+    if (pow(planet.r + this.r, 2) < pow(planet.pos.x - this.pos.x, 2) + pow(planet.pos.y - this.pos.y, 2) || !planet.enabled || !this.enabled) {
+      return;
+    }
+    if (this.mass > planet.mass) {
+      this.vel.add(planet.vel.mult(planet.mass / this.mass));
+      this.mass += planet.mass;
+      this.r = this.mass / 2;
+      planet.enabled = false;
+    }
+    else {
+      planet.vel.add(this.vel.mult(this.mass / planet.mass));
+      planet.mass += this.mass;
+      planet.r = planet.mass / 2;
+      this.enabled = false;
+    }
+  }
+}
