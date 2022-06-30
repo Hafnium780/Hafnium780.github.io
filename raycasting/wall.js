72d7d51165a14314231feb04d787f1ba26c47179
Hafnium780 <97712621+Hafnium780@users.noreply.github.com>
2022-06-29 18:15:39 -0700

    Create wall.js

---
+++ raycasting/wall.js
@@ -0,0 +1,12 @@
+class Wall {
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+  
+  show() {
+    line(this.x1, h-this.y1, this.x2, h-this.y2);
+  }
+}
