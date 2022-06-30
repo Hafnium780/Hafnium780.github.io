a593a9868156ecfbee7d91d8f8a28ccb5cdd1e99
Hafnium780 <97712621+Hafnium780@users.noreply.github.com>
2022-06-29 18:31:33 -0700

    Update sketch.js

--- minesweeper3d/sketch.js
+++ minesweeper3d/sketch.js
@@ -57,6 +57,8 @@ function reset() {
       }
     }
   }
+  layernum.remove();
+  inst.remove();
   layernum = createDiv('Layer: 1/' + zs);
   inst = createDiv('The middle row of each cell shows the current layer, the ones below and above correspond to the layers below and above. You only interact with the cells in the current layer. <br> <b>Left click </b>to mark as a flag <br> <b>Shift + left click </b>to reveal <br> <b>Left click </b>a revealed square to chord <br> Use the <b>up and down arrow keys </b>to move through the layers <br> Press <b>space </b>to restart <br> Cells with no neighboring mines are automatically chorded');
  }
