0b4dda58bda0cad1151904c9b3d8263d288a483f
Hafnium780 <97712621+Hafnium780@users.noreply.github.com>
2022-10-25 19:47:38 -0700

    Update nn.js

--- car/nn.js
+++ car/nn.js
@@ -161,12 +161,19 @@ class NeuralNetwork {
     return new NeuralNetwork(this);
   }
 
-  // Accept an arbitrary function for mutation
-  mutate(func) {
-    this.weights_ih.map(func);
-    this.weights_ho.map(func);
-    this.bias_h.map(func);
-    this.bias_o.map(func);
+  mutate(rate) {
+    function mutate(val) {
+      if (Math.random() < rate) {
+        // return 2 * Math.random() - 1;
+        return val + randomGaussian(0, 0.1);
+      } else {
+        return val;
+      }
+    }
+    this.weights_ih.map(mutate);
+    this.weights_ho.map(mutate);
+    this.bias_h.map(mutate);
+    this.bias_o.map(mutate);
   }
 
 
