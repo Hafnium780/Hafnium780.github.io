2def7d10e22eeebdae2f9e254bc3fc116e145dc5
Hafnium780 <97712621+Hafnium780@users.noreply.github.com>
2023-04-02 10:26:26 -0700

    Add files via upload

---
+++ physic/script.js
@@ -0,0 +1,62 @@
+let canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let ctx = canvas.getContext("2d");
+
+let mouseX = 0;
+let mouseY = 0;
+let v, a;
+let k = 0.2;
+let g = 6;
+
+canvas.addEventListener("mousemove", (e) => {
+	mouseX = e.offsetX;
+	mouseY = e.offsetY;
+	let tmpCoordinate = inverseTransform(mouseX, mouseY);
+	v =
+		Math.sqrt(
+			tmpCoordinate.x * tmpCoordinate.x + tmpCoordinate.y * tmpCoordinate.y
+		) / 3;
+	a =
+		tmpCoordinate.x == 0
+			? Math.PI / 2
+			: Math.atan(tmpCoordinate.y / tmpCoordinate.x);
+});
+
+function update() {
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "white";
+
+	let tmpCoordinate;
+
+	ctx.beginPath();
+	tmpCoordinate = transform(0, 0);
+	ctx.arc(tmpCoordinate.x, tmpCoordinate.y, 10, 0, Math.PI * 2);
+	ctx.fill();
+
+	let vk = v / k;
+	let vkcos = vk * Math.cos(a);
+	let vksin = vk * Math.sin(a);
+	let gk = g / k;
+	let vksingkk = vksin + gk / k;
+	for (let t = 0; t < 100; t += 0.1) {
+		let e = 1 - Math.exp(-k * t);
+		let x = vkcos * e;
+		let y = vksingkk * e - gk * t;
+		ctx.beginPath();
+		tmpCoordinate = transform(x, y);
+		ctx.arc(tmpCoordinate.x, tmpCoordinate.y, 2, 0, Math.PI * 2);
+		ctx.fill();
+	}
+}
+
+function transform(x, y) {
+	return { x: x, y: canvas.height - y };
+}
+
+function inverseTransform(x, y) {
+	return { x: x, y: canvas.height - y };
+}
+
+setInterval(update, 1000 / 60);
