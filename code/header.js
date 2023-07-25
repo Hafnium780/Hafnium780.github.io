2582b5a6be5e3755eb205ec5ef67818b3fa638b2
Hafnium780 <allenli780@gmail.com>
2023-07-25 12:15:32 -0700

    morse + sidebar save

--- code/header.js
+++ code/header.js
@@ -3,4 +3,15 @@
   headerDiv.id = "header";
   document.body.prepend(headerDiv);
   headerDiv.innerHTML = await (await fetch("../header.html")).text();
+
+  if (sessionStorage.getItem("sidebar-open") == "true")
+    document.getElementById("menu-toggle-checkbox").checked = true;
+  document
+    .getElementById("menu-toggle-checkbox")
+    .addEventListener("change", () => {
+      sessionStorage.setItem(
+        "sidebar-open",
+        document.getElementById("menu-toggle-checkbox").checked
+      );
+    });
 })();
