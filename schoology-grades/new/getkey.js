76d9772b518653ff34fa3d59ba46a325558828fe
Hafnium780 <allenli780@gmail.com>
2024-01-14 21:04:40 -0800

    ‍

--- schoology-grades/new/getkey.js
+++
@@ -1,6 +0,0 @@
-const key = document.getElementById("edit-current-key");
-const secret = document.getElementById("edit-current-secret");
-
-if (key) chrome.storage.local.set({ sg_key: key.value });
-if (secret && secret.value.indexOf("*") === -1)
-  chrome.storage.local.set({ sg_signature: secret.value });
